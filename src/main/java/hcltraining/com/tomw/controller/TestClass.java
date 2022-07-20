2c911f278c87b614394b7708f44c30634a02a4a7
Tommy Wang <tommy.wang2000@gmail.com>
2022-07-20 09:46:25 -0500

    Saved Information Java Training Day 1

---
+++ src/main/java/hcltraining/com/tomw/controller/TestClass.java
@@ -0,0 +1,13 @@
+package hcltraining.com.tomw.controller;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+		
+		int i = Integer.parseInt(args[0]);
+		System.out.println(i+3);
+		System.out.println(i+args[1]);
+		System.out.println(args[2]);
+	}
+}
